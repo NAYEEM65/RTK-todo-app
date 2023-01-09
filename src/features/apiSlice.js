6f811a6f09c3a1979824842db03f1c11c2cbd1f6
NAYEEM65 <mathnayeem054@gmail.com>
2023-01-09 23:07:16 +0600

    server changed

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://json-todo-api-nayeem.herokuapp.com',
+        baseUrl: 'https://data-server-yk0y.onrender.com',
     }),
     tagTypes: ['allTodos'],
     endpoints: (builder) => ({
